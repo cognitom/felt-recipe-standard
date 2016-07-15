e088314c9a9f3d213dc6364f2dd002451d16e5f0
Tsutomu Kawamura <cognitom@gmail.com>
2016-07-16 01:38:51 +0900

    First commit

---
+++ felt.config.js
@@ -0,0 +1,40 @@
+'use strict'
+
+/**
+ * Standard recipe for Felt
+ */
+
+const
+  rollup = require('felt-rollup'),
+  buble = require('rollup-plugin-babel'),
+  resolve = require('rollup-plugin-node-resolve'),
+  commonjs = require('rollup-plugin-commonjs'),
+  postcss = require('felt-postcss'),
+  postcssImport = require('postcss-import'),
+  autoprefixer = require('autoprefixer')
+
+module.exports = {
+  // default handlers for each extension
+  handlers: {
+    '.js': rollup({
+      plugins: [
+        resolve({ jsnext: true,  main: true, browser: true }),
+        commonjs(),
+        babel({
+          babelrc: false,
+          presets: ['es2015-rollup']
+        })
+      ],
+      sourceMap: true
+    }),
+    '.css': postcss({
+      plugins: [
+        postcssImport(),
+        autoprefixer()
+      ],
+      options: {
+        map: { inline: false }
+      }
+    })
+  }
+}
